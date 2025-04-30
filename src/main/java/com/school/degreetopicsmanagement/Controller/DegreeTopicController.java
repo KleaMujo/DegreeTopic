7a846f63d89d3286c08df2c4f2fc9e95cc79ba15
KleaMujo <kleamujo24@gmail.com>
2025-04-30 17:04:47 +0200

    k

--- src/main/java/com/school/degreetopicsmanagement/Controller/DegreeTopicController.java
+++ src/main/java/com/school/degreetopicsmanagement/Controller/DegreeTopicController.java
@@ -2,9 +2,13 @@ package com.school.degreetopicsmanagement.Controller;
 
 import com.school.degreetopicsmanagement.DataObjects.DegreeTopicDTO;
 import com.school.degreetopicsmanagement.Model.DegreeTopic;
+import com.school.degreetopicsmanagement.Model.User;
 import com.school.degreetopicsmanagement.Repository.DegreeTopicRespository;
-import lombok.Getter;
+import com.school.degreetopicsmanagement.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +24,13 @@ public class DegreeTopicController {
  @Autowired
  DegreeTopicRespository degreeTopicRespository;
 
+ @Autowired
+ UserRepository userRepository;
+
 
     @GetMapping(value="/addDegreeTopic")
     public ModelAndView  addDegreeTopic(ModelAndView modelAndView){
-        modelAndView.setViewName("addDegreeTopic");
+        modelAndView.setViewName("Teacher/addDegreeTopic");
      return modelAndView;
     }
 
@@ -31,16 +38,23 @@ public class DegreeTopicController {
     @ResponseBody
     public void addDegreeTopic(@RequestBody DegreeTopicDTO degreeTopicDTO){
         DegreeTopic degreeTopic = new DegreeTopic();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails authenticatedUser = (UserDetails) authentication.getPrincipal();
+        User user = userRepository.findByUsername(authenticatedUser.getUsername());
+        System.out.println(user.getRole() + " role - "  + user.getId());
         degreeTopic.setTitle(degreeTopicDTO.getTitleDTO());
         degreeTopic.setDescription(degreeTopicDTO.getDescriptionDTO());
-        degreeTopicRespository.save(degreeTopic);
+        degreeTopic.setTeacher(user);
+         degreeTopicRespository.save(degreeTopic);
     }
 
     @GetMapping(value="/showAllDegreeTopic")
     public ModelAndView  showAllDegreeTopic(ModelAndView modelAndView){
-         List<DegreeTopic> degreeTopics = degreeTopicRespository.findAll();
+          List<DegreeTopic> degreeTopics = degreeTopicRespository.findAll();
          modelAndView.addObject("degreeTopics", degreeTopics);
-        modelAndView.setViewName("degreeTopicList");
-        return modelAndView;
+         modelAndView.setViewName("degreeTopicList");
+         return modelAndView;
     }
+
+
 }
--- src/main/java/com/school/degreetopicsmanagement/Controller/HomeController.java
+++ src/main/java/com/school/degreetopicsmanagement/Controller/HomeController.java
@@ -1,11 +1,30 @@
 package com.school.degreetopicsmanagement.Controller;
 
 import javax.servlet.http.HttpServletRequest;
+
+import com.school.degreetopicsmanagement.Model.DegreeTopic;
+import com.school.degreetopicsmanagement.Model.User;
+import com.school.degreetopicsmanagement.Repository.DegreeTopicRespository;
+import com.school.degreetopicsmanagement.Repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private final UserRepository userRepository;
+    private final DegreeTopicRespository degreeTopicRespository;
+
+    public HomeController(UserRepository userRepository, DegreeTopicRespository degreeTopicRespository) {
+        this.userRepository = userRepository;
+        this.degreeTopicRespository = degreeTopicRespository;
+    }
 
     @GetMapping("/")
     public String homePage(HttpServletRequest httpServletRequest) {
@@ -16,6 +35,36 @@ public class HomeController {
             return "login";
         }
     }
+
+    @GetMapping(value = "/studentList")
+    public ModelAndView studentList(HttpServletRequest httpServletRequest, ModelAndView modelAndView) {
+        List<User> userList = userRepository.findAll();
+         for(User user : userList) {
+             if(user.getRole().equals("student")) {
+                 modelAndView.addObject("student", user);
+             }
+         }
+          List<DegreeTopic> degreeTopics = degreeTopicRespository.findAll();
+          modelAndView.addObject("degreeTopics", degreeTopics);
+         modelAndView.setViewName("studentList");
+        return modelAndView;
+    }
+
+    @GetMapping(value="/topicsStudent")
+    public ModelAndView topicsStudent(HttpServletRequest httpServletRequest, ModelAndView modelAndView) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails authenticatedUser = (UserDetails) authentication.getPrincipal();
+        User user = userRepository.findByUsername(authenticatedUser.getUsername());
+
+        modelAndView.addObject("studentId", user.getId());
+        List<DegreeTopic> degreeTopics = degreeTopicRespository.findAll();
+        modelAndView.addObject("degreeTopics", degreeTopics);
+
+        modelAndView.setViewName("Student/degreeListStudent");
+        return modelAndView;
+        }
+
+
 
 
 
